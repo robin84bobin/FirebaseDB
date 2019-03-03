a1a9195aae83a86f7fc8cba70ad6433b76e9da7d
robin84bobin <robin84bobin@gmail.com>
2019-03-03 12:37:32 +0300

    command sequence

--- Assets/Scripts/Commands/Command.cs
+++ Assets/Scripts/Commands/Command.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace Commands
 {
@@ -17,54 +16,6 @@ namespace Commands
         {
             OnComplete.Invoke();
             Release();
-        }
-    }
-
-    public class CommandSequence : Command
-    {
-        public event Action<float> OnProgress = delegate { };
-        private readonly Command[] _commands;
-
-        /// <summary>
-        /// do sequence of commands
-        /// </summary>
-        /// <param name="commands"></param>
-        public CommandSequence(params Command[] commands)
-        {
-            _commands = commands;
-        }
-        
-
-        public override void Execute()
-        {
-            for (var i = 0; i < _commands.Length; i++)
-            {
-                if (i < _commands.Length - 1)
-                {
-                    var nextCommand = _commands[i + 1];
-                    var currentCommand = i;
-                    _commands[i].OnComplete += () =>
-                    {
-                        CommandManager.Execute(nextCommand);
-                        OnProgress.Invoke((float)(currentCommand+1)/_commands.Length);
-                    };
-                }
-                else if (i == _commands.Length - 1)
-                    _commands[i].OnComplete += () =>
-                    {
-                        Complete();
-                        OnProgress.Invoke(1f);
-                    };
-                    
-            }
-
-            CommandManager.Execute(_commands.First());
-        }
-
-        protected override void Release()
-        {
-            base.Release();
-            OnProgress = null;
         }
     }
 }
